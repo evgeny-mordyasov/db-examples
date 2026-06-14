618c21f48daba323653246b17fa2d82047d6f017
evgeny-mordyasov <michisig11i7@gmail.com>
2026-06-14 18:44:41 +0700

    Externalize Postgres address for repository tests

--- testcontainers-support/src/main/java/ru/gold/ordance/jdbc/examples/testcontainers/TestcontainersPostgresConfiguration.java
+++ testcontainers-support/src/main/java/ru/gold/ordance/jdbc/examples/testcontainers/TestcontainersPostgresConfiguration.java
@@ -1,23 +1,23 @@
 package ru.gold.ordance.jdbc.examples.testcontainers;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.testcontainers.containers.ContainerState;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import javax.sql.DataSource;
+import java.util.function.Supplier;
 
 import static org.testcontainers.utility.DockerImageName.parse;
 
-@TestConfiguration(proxyBeanMethods = false)
+@Configuration(proxyBeanMethods = false)
 public class TestcontainersPostgresConfiguration {
 
     private static final String POSTGRES = "postgres:17-alpine";
-    private static final String POSTGRES_URL = "spring.datasource.url";
-    private static final String POSTGRES_USERNAME = "spring.datasource.username";
-    private static final String POSTGRES_PASSWORD = "spring.datasource.password";
+    private static final String PG_CONN_STR = "postgres-addr";
     private static final String MIGRATION_LOCATION = "classpath:db/migration";
 
     @Bean
@@ -28,11 +28,11 @@ public class TestcontainersPostgresConfiguration {
 
     @Bean
     DynamicPropertyRegistrar postgresProperties(PostgreSQLContainer postgresContainer) {
-        return properties -> {
-            properties.add(POSTGRES_URL, postgresContainer::getJdbcUrl);
-            properties.add(POSTGRES_USERNAME, postgresContainer::getUsername);
-            properties.add(POSTGRES_PASSWORD, postgresContainer::getPassword);
-        };
+        return properties -> properties.add(PG_CONN_STR, hostAndFirstMappedPort(postgresContainer));
+    }
+
+    private static Supplier<Object> hostAndFirstMappedPort(ContainerState cs) {
+        return () -> cs.getHost() + ":" + cs.getFirstMappedPort();
     }
 
     @Bean
